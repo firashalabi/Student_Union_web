7bcce4072f942cf0bffb92d62688c265e14865b0
firas halabi <firashalabi3@gmail.com>
2024-02-04 01:13:18 +0200

    Accordion Design Modifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,39 @@
 'use client'
 import Image from 'next/image'
-import {
-Accordion,
-AccordionContent,
-AccordionItem,
-AccordionTrigger,
-} from "@/app/components/ui/accordion"
-
 import { accordion  } from "./constants"
 import Navbar from '@/app/components/shared/Navbar'
-import backgroundImage from'@/public/1100104.jpg'
-import { EmbeddedTweet, TweetProps } from 'react-tweet';
-import { Tweet } from './components/ui/Tweet';
-import { components } from './components/ui/TweetEmbed';
-import { tweetIds } from '@/lib/utils';
-import { getTweet } from 'react-tweet/api';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/app/components/ui/accordion'
+import { Button } from './components/ui/button'
 
 
 export default function Home() {
-//   const MyTweet = ({ id }: { id: string }) => (
-//     <Tweet id={id} components={components} />
-//   )
-
 
   
   return (
     <>
       <div>
       <Navbar />
+
       </div>
     
     <main className="min-h-screen w-full text-center ">
-          <section id='main'  className='flex-row text-center w-full'>
-          <div className="flex-col ">
-            <span className='font-bold '>الاتحاد الطلابي العام<br />
+          <section id='main'  className=' text-center w-full'>
+          <div className=" flex-row items-center justify-center mx-auto ">
+            <h2 className='font-bold '>الاتحاد الطلابي العام<br />
                 وثيقة الانتساب السياسيّة
-              </span>
-              <p className="my-[4%]  text-wrap w-[300px] text-right">
+              </h2>
+              <p className="my-[4%] mx-auto text-wrap w-[350px] md:w-[500px] ">
               تيمنا بالاتحاد الوطني للطلاب مؤسسين الجامعية اللبنانية، اتحادا نقابيا لاجل الطلاب والمجتمع واسكتمالا لنضال الحركة الطلابيا تاريخيا بين من بدء نضال التأسيس للجامعة الوطنية حتى اليوم في معركة الحفاظ على حق التعليم وبناء المجتمع وحمايته من التفتيت.
               </p>
           </div>
-      <div className="my-[4%] w-[300px] justify-center ">
+      <div className="my-[4%] w-[400px] md:w-[600px] mx-auto ">
         {accordion.map(item =>(
-          <Accordion type="single" collapsible className="w-full " key="key.id">
+          <Accordion type="single" collapsible className="w-full" key="key.id">
               <AccordionItem value="item.id" >
 
                 <AccordionTrigger>{item.title}</AccordionTrigger>
 
-                  <AccordionContent>{item.content}</AccordionContent>
+                  <AccordionContent className=' h-[200px] line-clamp-4 overflowx-hidden hover:overflow-y-scroll scrollbar scrollbar-thin p-4'> {item.content}</AccordionContent>
                 
               </AccordionItem>
           </Accordion>
@@ -57,6 +43,7 @@ export default function Home() {
     </section>
 
     <section>   
+      
     {/* {tweet.map((tweet) => (
   <EmbeddedTweet tweet={tweet.tweetId} key={tweet.id} /> */}
 {/* ))} */}
